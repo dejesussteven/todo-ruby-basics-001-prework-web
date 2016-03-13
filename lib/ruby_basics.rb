b7b01dd36e5f9bf00b8619cc8c47c1b49a837758
dejesussteven <stevendejesus90@gmail.com>
2016-03-13 19:02:00 +0100

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,36 @@
 def division(num1, num2)
- 
+   sum = num1/num2
+   sum
 end
+ division(42,7)
 
 def assign_variable(value)
-  
+  value
 end
+assign_variable("Bob")
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
+argue("I'm right and you are wrong!")
 
-def greeting
-
+def greeting(greeting, name)
 end
+greeting("Hi there, ", "Bobby!")
+
 
 def return_a_value
-
+  "Nice"
 end
+return_a_value
 
 def last_evaluated_value
-
+    "expert"
 end
+last_evaluated_value
 
-def pizza_party
-
+def pizza_party(type= "cheese")
+return type
 end
+pizza_party
+pizza_party("pepperoni")
